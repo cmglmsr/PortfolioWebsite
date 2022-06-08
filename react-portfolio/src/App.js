cea09f8970a6c4206d01f9a84e7975ec706e265f
unknown <cemg@server.com>
2022-06-09 00:30:14 +0300

    trying

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -12,9 +12,9 @@ function App() {
     <Routes>
       <Route path="https://cmglmsr.github.io/PortfolioWebsite/" element={<Layout />}>
       <Route path="https://cmglmsr.github.io/PortfolioWebsite/" index element={<Home />} />
-      <Route path="https://cmglmsr.github.io/about" element={<About />} />
-      <Route path="https://cmglmsr.github.io/contact" element={<Contact />} />
-      <Route path="https://cmglmsr.github.io/research" element={<Research />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/research" element={<Research />} />
       </Route>
     </Routes>
     </>
