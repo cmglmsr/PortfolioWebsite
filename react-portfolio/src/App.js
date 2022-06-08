493716bae91cbe150cdf938bf5e48430ea251a53
unknown <cemg@server.com>
2022-06-09 00:27:11 +0300

    trying

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/PortfolioWebsite" element={<Layout />}>
-      <Route path="/PortfolioWebsite" index element={<Home />} />
-      <Route path="PortfolioWebsite/about" element={<About />} />
-      <Route path="PortfolioWebsite/contact" element={<Contact />} />
-      <Route path="PortfolioWebsite/research" element={<Research />} />
+      <Route path="https://cmglmsr.github.io/PortfolioWebsite/" element={<Layout />}>
+      <Route path="https://cmglmsr.github.io/PortfolioWebsite/" index element={<Home />} />
+      <Route path="https://cmglmsr.github.io/PortfolioWebsite/about" element={<About />} />
+      <Route path="https://cmglmsr.github.io/PortfolioWebsite/contact" element={<Contact />} />
+      <Route path="https://cmglmsr.github.io/PortfolioWebsite/research" element={<Research />} />
       </Route>
     </Routes>
     </>
