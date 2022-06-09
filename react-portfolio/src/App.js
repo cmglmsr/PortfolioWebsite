0aa917c8d2bac1878a92753429e30dec96af136c
unknown <cemg@server.com>
2022-06-10 00:09:17 +0300

    tab name changed

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import './App.scss';
 import Home from './components/Home'
 import { Routes, Route } from 'react-router-dom'
@@ -5,6 +6,8 @@ import Layout from './components/Layout'
 import About from './components/About'
 import Contact from './components/Contact'
 import Research from './components/Research'
+
+
 
 function App() {
   return (
