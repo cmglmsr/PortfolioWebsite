03d1615fdffc75083f1978272394afbb9c3e7936
unknown <cemg@server.com>
2022-07-14 16:06:11 +0300

    abt page completed

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -52,10 +52,26 @@ const About = () =>  {
                     </div>
                     <div className='display-item-mid'>
                         <h1>Microsoft Certified Azure Network Engineer Associate</h1>
+                        <p> While doing my internship at SunExpress as a network and cybersecurity engineer, I also found chance to
+                            learn about the fundamentals of cloud computing,, networking and security. One of the objectives we were
+                            working towards was to migrate the on-premises resources to cloud. Working on this task allowed me to learn
+                            many things about Microsoft Azure. I wanted to further my knowled   ge on this topic by working on it myself and
+                            apply for the certification exam of Microsoft. With the help of my experienced supervisors, I succesfully passed
+                            the exam and earned the Azure Network Engineer Associate certification. I am open to working as a cloud network 
+                            engineer or cloud solutions architect!<br/><br/>
+                            <a href="https://www.credly.com/badges/cadd7d0b-8c5b-428a-8b06-de05677f0980/public_url"> See my certification {'>'}{'>'} </a>
+                        </p>
                         <img src={az700} alt=''></img>
                     </div>
                     <div className='display-item'>
                         <h1>American Collegiate Institute IBDP & 4th in Academic Standing</h1>
+                        <p> I graduated from American Collegiate Institute with an International Baccalaureate (IB) Math-Science diploma as well as a Turkish High
+                            School diploma. I was ranked the 4th among all students in my school, the 3rd among all IB students and the 2nd among the IB Math-Science
+                            students. In the course of my high school education, I was awarded merit scholarships two times for being in top three in academic standing.
+                            If you wish to read more about the IB Diploma Programme offered by American Collegiate Institute, click the link below. 
+                            <br/><br/>
+                            <a href="https://www.aci.k12.tr/en/Icerik/ib-diploma-programi/410/"> Go to webpage {'>'}{'>'} </a>
+                        </p>
                         <img src={aci} alt=''></img>
                     </div>
                 </div>
@@ -68,8 +84,8 @@ const About = () =>  {
                 <div className="dropdownmenu">
                     <Dropdown title="Education" item1="American Collegiate Institute: 2016-2020" item2="Bilkent University: 2020 - Present"/>
                     <Dropdown title="Work Experience" item1="SunExpress - Networking & Cybersecurity Intern: 2022" item2="Kocaeli Derince State Hospital - Biochemistry Lab Intern: 2018"/>
-                    <Dropdown title="Computer Skills" item1="Intermediate C++ and Java Coding" item2="Python for Cybersecurity / Ethical Hacking + Experience with Kali Linux and Network Libraries" 
-                    item3="Frontend Development with React.js, HTML and SCSS" item4="Extensive Experience with Microsoft Word, Microsoft Excel and Google Docs"/>
+                    <Dropdown title="Computer Skills" item1="Intermediate C++ and Java Coding" item2="Python for Networking / Automation / Cybersecurity / Ethical Hacking + Experience with Kali Linux and Network Libraries" 
+                    item3="Frontend Development with React.js, HTML and SCSS" item4="Extensive Experience with Microsoft Azure, Microsoft Word, Microsoft Excel and Google Docs"/>
                     <Dropdown title="Languages" item1="C1 English: IELTS 8.0" item2="A2 German: Goethe Zertifikat A2"/>
                     <Dropdown title="Projects & Research" item1="Health and Education Foundation Project Competition Winner - Rainwater Harvest Project: 2018" 
                     item2="Android Calendar Application - Calendy: Coded in Android Studio with Java and XML" item3="Research Paper - Detection of Face Morph Attacks on Facial ID Verification Systems: 2021"/>
